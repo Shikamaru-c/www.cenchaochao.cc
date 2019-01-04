f0f7be34b126d86b0a4a7b2a35e097d1ca7cc432
CENCHAOCHAO <cenchaochao@outlook.com>
2019-01-04 11:43:11 +0800

    fix: server port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 // const history = require('connect-history-api-fallback')
 
+const PORT = 5058
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, '../build/')))
@@ -10,6 +12,6 @@ app.get('/deploy', (req, res) => {
   console.log('/deploy')
 })
 
-app.listen(5058, () => {
-  console.log(`the server is on port 3000`)
+app.listen(PORT, () => {
+  console.log(`the server is on port ${PORT}`)
 })
